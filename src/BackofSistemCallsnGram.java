a132435cddaa5dc586dd3a807cc0c875cdd17115
Hui(Philip) Yang <csseryang@users.noreply.github.com>
2016-11-06 11:31:54 -0800

    add how to compute TF_IDF value

--- src/BackofSistemCallsnGram.java
+++ src/BackofSistemCallsnGram.java
@@ -1,3 +1,5 @@
+package helloworld;
+
 import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -9,7 +11,7 @@ import java.util.Set;
 
 public class BackofSistemCallsnGram {
 	//public static File folder = new File("/Users/MrTiriti/Downloads/testMipsSep26/analysis/result/SystemCallTraces");
-	public static File folder = new File("C:/Users/lluna/Desktop/mio/Desktop/UCR/Fall16/Network Routing/Project/testMipsSep26/analysis/result/SystemCallTraces");
+	public static File folder = new File("/Users/hyang027/Desktop/240/data");
 	public static List<String> fileList = new ArrayList<String>();
 	public static Hashtable<String,Integer> generalFrequencies = new Hashtable<String,Integer>();
 	
@@ -85,12 +87,14 @@ public class BackofSistemCallsnGram {
 				systemCallTraceFreq.put(newString, systemCallTraceFreq.get(newString)+1);
 			}else{
 				systemCallTraceFreq.put(newString, 1);	
+				
+				if(generalFrequencies.containsKey(newString)){
+					generalFrequencies.put(newString, generalFrequencies.get(newString)+1);
+				}else{
+					generalFrequencies.put(newString, 1);	
+				}
 			}
-			if(generalFrequencies.containsKey(newString)){
-				generalFrequencies.put(newString, generalFrequencies.get(newString)+1);
-			}else{
-				generalFrequencies.put(newString, 1);	
-			}
+			
 		}
 		return systemCallTraceFreq;
 	}
@@ -105,13 +109,28 @@ public class BackofSistemCallsnGram {
 		Set<String> keyHashTable=hashtable.keySet();
 		for(String item:keyHashTable){
 			int freq=hashtable.get(item);
-			double newFreq=Math.log10((int)(freq+1));
+			double newFreq=Math.log10((freq+1.0) ); 
 			tf.put(item, newFreq);
 		}
 		return tf;
 	}
 	
+	/*
+	 * IDF, part of scaling. 
+	 */
+	public static Hashtable<String,Double> IDF(){
+		Hashtable<String,Double> idf=new Hashtable<String,Double>();
+		
 	
+		int N=fileList.size();
+		Set<String> keyHashTable=generalFrequencies.keySet();
+		for(String item:keyHashTable){
+			int freq=generalFrequencies.get(item);
+			double newFreq=Math.log10( (1.0+N)/(freq+1.0) ) +1.0;
+			idf.put(item, newFreq);
+		}
+		return idf;
+	}
 	/*
 	 * Main 
 	 */
@@ -120,15 +139,38 @@ public class BackofSistemCallsnGram {
 		ArrayList<ArrayList<String>> allTraces=new ArrayList<ArrayList<String>>();
 		ArrayList<Hashtable<String,Integer>> allFrequencies=new ArrayList<Hashtable<String,Integer>>();
 		ArrayList<Hashtable<String,Double>> allScaling=new ArrayList<Hashtable<String,Double>>();
+		
+	
+		
 		for(String item:fileList){
 			ArrayList<String> systemCallTrace=importCSV(item);
 			Hashtable<String,Integer> systemCallTraceFreq=nGram(systemCallTrace,2);
 			Hashtable<String,Double> systemCallTraceTF=TF(systemCallTraceFreq);
+		   
+		 
 			allTraces.add(systemCallTrace);
 			allFrequencies.add(systemCallTraceFreq);
 			allScaling.add(systemCallTraceTF);
 		}
 		
+
+		Hashtable<String,Double> systemCallTraceIDF= IDF();
+		
+		System.out.print(systemCallTraceIDF);
+		for(Hashtable<String,Double> eachTF:allScaling)
+		{
+			Set<String> keyHashTable=eachTF.keySet();
+			for(String ngram:keyHashTable)
+			{
+				
+				double TFIDF_value = eachTF.get(ngram)*systemCallTraceIDF.get(ngram);
+				eachTF.put(ngram, TFIDF_value);
+			}
+		}
+		System.out.println(allScaling);
+		
+		System.out.println(systemCallTraceIDF);
+	
 	}
 	
 }
